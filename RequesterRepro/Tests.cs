15ad0bf396cd74314e100a4be7bd271c117d0227
Adam Shelley <ashelley@retool.co>
2016-11-01 16:59:37 -0700

    initial commit

---
+++ RequesterRepro/Tests.cs
@@ -0,0 +1,19 @@
+﻿using Requester.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RequesterRepro
+{
+    public class Tests
+    {
+        [Fact]
+        public void ShouldTest()
+        {
+            When.Head("http://www.google.com").TheResponse(should => should.BeSuccessful());
+        }        
+    }
+}
